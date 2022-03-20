05b6b053d506384fadca7dcc1c2d632e89576e35
David Silber <ddragontech@gmail.com>
2022-03-20 11:07:53 -0400

    Made WallButton actually toggle the state of drawing walls - test

--- src/test/kotlin/tests/WallButtonTest.kt
+++ src/test/kotlin/tests/WallButtonTest.kt
@@ -3,8 +3,13 @@ package tests
 import WallButton
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
+import org.junit.Assert.assertThat
 import org.junit.Rule
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class WallButtonTest {
   @get:Rule
@@ -13,8 +18,9 @@ class WallButtonTest {
   @Test
   fun testButtonText() {
     composeTestRule.setContent {
+      var drawingWalls = false
 //      Text("Wall")
-      WallButton()
+      WallButton(drawingWalls, {} )
     }
 
     val button = composeTestRule.onNodeWithText("Wall")
@@ -22,6 +28,33 @@ class WallButtonTest {
     button.assertExists()
 //    button.assertIsDisplayed()  // Not implemented
 //    button.assertDoesNotExist()  // not true
+  }
+
+  // Fails because printToLog is not implemented as of 2022-03-20
+  // found printToLog in https://developer.android.com/jetpack/compose/testing
+//  @Test
+//  fun `button is red when drawingWalls is true`() {
+//    composeTestRule.setContent {
+//      var drawingWalls = false
+//      WallButton(drawingWalls, {} )
+//    }
+//
+////    val button = composeTestRule.onNodeWithText("Wall")
+//
+//    composeTestRule.onRoot().printToLog("TAG")
+//  }
+
+  @Test
+  fun `button invokes function on click`() {
+    var drawingWalls: Boolean = false
+    composeTestRule.setContent {
+      WallButton(drawingWalls, {drawingWalls = !drawingWalls} )
+    }
+    val button = composeTestRule.onNodeWithText("Wall")
+
+    button.performClick()
+
+    assertTrue(drawingWalls)
   }
 
 }
